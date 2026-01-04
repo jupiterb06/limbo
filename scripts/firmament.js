601b30b82650b3b5fd14f2d7c43f5ccc452c2b9a
jupiterb06 <jupiterbhanot@gmail.com>
2026-01-04 04:56:44 +0000

    firmament script

---
+++ scripts/firmament.js
@@ -0,0 +1,14 @@
+$("#ok").click(function (e) { 
+    $("#p1").removeClass("hidden");
+    $("#btn1").removeClass("hidden");
+});
+
+$("#btn1").click(function (e) {
+    $("#p2").removeClass("hidden");
+    $("#btn2").removeClass("hidden");
+});
+
+$("#btn2").click(function (e) { 
+    $("#p3").removeClass("hidden");
+    $("#btn3").removeClass("hidden");    
+});
